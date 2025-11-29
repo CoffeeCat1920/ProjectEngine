70636bc4b222fcb5bd46f868a0c5324dab21ef37
CoffeeCat1920 <H0neyCatCoffeeGo@proton.me>
2025-11-29 18:53:35 +0500

    fixed gitignore

--- build/engine/CMakeFiles/engine.dir/src/cameraController/cameraController.cpp.o.d
+++ build/engine/CMakeFiles/engine.dir/src/cameraController/cameraController.cpp.o.d
@@ -3,4 +3,4 @@ engine/CMakeFiles/engine.dir/src/cameraController/cameraController.cpp.o: \
  /usr/include/stdc-predef.h \
  /home/ok/Code/ProjectEngine/engine/include/cameraController/cameraController.hpp \
  /home/ok/Code/ProjectEngine/vendor/raylib/include/raylib.h \
- /usr/lib/gcc/x86_64-redhat-linux/15/include/stdarg.h
+ /usr/lib/gcc/x86_64-pc-linux-gnu/15.2.1/include/stdarg.h
